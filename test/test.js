7425bc1d36686fc630bb7bb03a5abbc425673184
Oluwakamiye Adelemoni <oluwakamiye.adelemoni@andela.com>
2016-04-25 17:32:46 +0100

    Initial Commit

---
+++ test/test.js
@@ -0,0 +1,7 @@
+describe("String Class Test Suite", function() {
+	describe("Random Test", function() {
+		it("should confirm test", function() {
+			expect(TestObject.name).toEqual('Kamiye');
+		});
+	});
+});
