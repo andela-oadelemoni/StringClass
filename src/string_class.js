1385bbc189335e7f634039102fcafd285816e654
Oluwakamiye Adelemoni <oluwakamiye.adelemoni@andela.com>
2016-04-26 13:27:45 +0100

    [Application Test] Add all tests for the use cases of the string class

--- src/string_class.js
+++ src/string_class.js
@@ -1,3 +1,36 @@
-var TestObject = {
-	name: 'Kamiye'
+String.prototype.hasVowels = function() {
+
 };
+
+String.prototype.toUpper = function() {
+	
+};
+
+String.prototype.toLower = function() {
+
+};
+
+String.prototype.ucFirst = function() {
+
+};
+
+String.prototype.isQuestion = function() {
+
+};
+
+String.prototype.words = function() {
+
+};
+
+String.prototype.wordCount = function() {
+
+};
+
+String.prototype.toCurrency = function() {
+
+};
+
+String.prototype.fromCurrency = function() {
+
+};
+
